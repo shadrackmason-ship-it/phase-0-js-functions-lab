6b9feba586bc116e15d27c4e0834a9ad7341f743
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-04-16 16:22:28 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = (discountPercentage / 100) * originalPrice;
+  return originalPrice - discount;
+}
+
+// Export (IMPORTANT for tests to work)
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
 
 
 
